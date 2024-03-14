90b73b1f3165b8c3b89e7664b403e65f05119138
Bikash Das <bikashd003@gmail.com>
2024-03-14 12:00:55 +0530

    fix bugs

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   dotenv.config({path:"../.env"});
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(process.env.PORT || 5000);
+  await app.listen(process.env.PORT || 5000,()=>console.log("server start at port",process.env.PORT));
 }
 bootstrap();
--- src/users.controller.ts
+++ src/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Req, Res, HttpStatus } from '@nestjs/common';
+import { Controller, Post, Get, Req, Res, HttpStatus } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 import * as jwt from 'jsonwebtoken';
 import { Model } from 'mongoose';
@@ -7,50 +7,79 @@ import { User, UserDocument } from './Models/User.model';
 
 @Controller('/user')
 export class UserController {
-  constructor(@InjectModel(User.name) private readonly userModel: Model<UserDocument>) {}
-
+  constructor(
+    @InjectModel(User.name) private readonly userModel: Model<UserDocument>,
+  ) {}
+  @Get('/health')
+  async healthCheck(@Res() res) {
+    return res.status(HttpStatus.OK).json({ message: 'Health Check' });
+  }
   @Post('/login')
   async login(@Req() req, @Res() res) {
     const { email, password } = req.body;
     if (!email || !password) {
-      return res.status(HttpStatus.BAD_REQUEST).json({ message: 'All fields are required' });
+      return res
+        .status(HttpStatus.BAD_REQUEST)
+        .json({ message: 'All fields are required' });
     }
     try {
       const user = await this.userModel.findOne({ email: email.trim() }).exec();
       if (!user) {
-        return res.status(HttpStatus.NOT_FOUND).json({ message: 'User not found' });
+        return res
+          .status(HttpStatus.NOT_FOUND)
+          .json({ message: 'User not found' });
       }
       const isMatch = await bcrypt.compare(password, user.password);
       if (!isMatch) {
-        return res.status(HttpStatus.UNAUTHORIZED).json({ message: 'Invalid credentials' });
+        return res
+          .status(HttpStatus.UNAUTHORIZED)
+          .json({ message: 'Invalid credentials' });
       }
-      const token = jwt.sign(user.toJSON(), "helloworld", {
+      const token = jwt.sign(user.toJSON(), 'helloworld', {
         expiresIn: '10d',
       });
-      res.status(HttpStatus.OK).json({ message: 'Login successful', token: token });
+      res
+        .status(HttpStatus.OK)
+        .json({ message: 'Login successful', token: token });
     } catch (err) {
       console.log('Error during login:', err);
-      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({ message: 'Internal server error' });
+      return res
+        .status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .json({ message: 'Internal server error' });
     }
   }
   @Post('/register')
-    async register(@Req() req, @Res() res) {
-      const {name, email, password } = req.body;
-      if (!name || !email || !password) {
-        return res.status(HttpStatus.BAD_REQUEST).json({ message: 'All fields are required' });
-      }
-      try {
-        const existedUser=await this.userModel.findOne({ email: email.trim() }).exec();
-        if (existedUser) {
-          return res.status(HttpStatus.BAD_REQUEST).json({ message: 'Email already exists' });
-        }
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const user = new this.userModel({name:name.trim(), email: email.trim(), password: hashedPassword });
-        await user.save();
-        res.status(HttpStatus.CREATED).json({ message: 'Registration successful' });
-      } catch (err) {
-        console.log('Error during registration:', err);
-        return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({ message: 'Internal server error' });
-      }
+  async register(@Req() req, @Res() res) {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+      return res
+        .status(HttpStatus.BAD_REQUEST)
+        .json({ message: 'All fields are required' });
     }
+    try {
+      const existedUser = await this.userModel
+        .findOne({ email: email.trim() })
+        .exec();
+      if (existedUser) {
+        return res
+          .status(HttpStatus.BAD_REQUEST)
+          .json({ message: 'Email already exists' });
+      }
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const user = new this.userModel({
+        name: name.trim(),
+        email: email.trim(),
+        password: hashedPassword,
+      });
+      await user.save();
+      res
+        .status(HttpStatus.CREATED)
+        .json({ message: 'Registration successful' });
+    } catch (err) {
+      console.log('Error during registration:', err);
+      return res
+        .status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .json({ message: 'Internal server error' });
+    }
+  }
 }
